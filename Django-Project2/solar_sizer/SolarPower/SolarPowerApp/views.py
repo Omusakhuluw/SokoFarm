c976cc884c299be52ef3961ff5337fd3d8b51147
omusakhulu <omusakhuluw@gmail.com>
2024-02-20 22:08:33 +0300

    updated urls.py and views.py

--- Django-Project2/solar_sizer/SolarPower/SolarPowerApp/views.py
+++ Django-Project2/solar_sizer/SolarPower/SolarPowerApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def SolarPowerApp(request):
+    return HttpResponse("Hello there, this is Omusakhulu!")
 
 # Create your views here.
